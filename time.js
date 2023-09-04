aaded6de3f2061e64ecca27a944d86642f666d4d
tracy1949 <63947743+tracy1949@users.noreply.github.com>
2023-09-04 18:22:58 +0800

    Update time.js

--- time.js
+++ time.js
@@ -5,26 +5,26 @@
 #每天 7:00-23:00 整点通知, 也可以自定义其他时间, 详情:https://community.nssurge.com/d/33-scripting
 
 [Script]
-cron "0 0/30 7-23 * * *" script-path=https://github.com/congcong0806/surge-list/raw/master/Script/time.js
+cron "0 7-23 * * *" script-path=https://github.com/congcong0806/surge-list/raw/master/Script/time.js
 
-*/ ["UTC", +0],["纽约", -5],["东京", +9],["巴黎", +2],["悉尼", +10]
+*/    ["UTC", +0],["纽约", -5],["东京", +9],["巴黎", +2],["悉尼", +10]
 
 Date.prototype.format = function(fmt) {
-	var date = {
-	        "M+": this.getMonth() + 1,
-	        "d+": this.getDate(),
-	        "h+": this.getHours(),
-	        "m+": this.getMinutes(),
-	        "s+": this.getSeconds(),
-	        "q+": Math.floor((this.getMonth() + 3) / 3),
-	        "S": this.getMilliseconds()
-	    };
+ var date = {
+         "M+": this.getMonth() + 1,
+         "d+": this.getDate(),
+         "h+": this.getHours(),
+         "m+": this.getMinutes(),
+         "s+": this.getSeconds(),
+         "q+": Math.floor((this.getMonth() + 3) / 3),
+         "S": this.getMilliseconds()
+     };
     if (/(y+)/i.test(fmt)) {
-    	fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+     fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
     }
     for (var k in date) {
         if (new RegExp("(" + k + ")").test(fmt)) {
-        	fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+         fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
         }
     }
     return fmt;
@@ -37,16 +37,16 @@ const areas = [
 time();
 
 async function time() {
-	var now = new Date(); 
-	var content = "";
-	var week = "            "+calcTime(+8)+" 星期" + "一二三四五六日".charAt(now.getDay());
-	content += week;
-	for (var i in areas) {
-		var u = valPlus(areas[i][1]);
+ var now = new Date(); 
+ var content = "";
+ var week = "          "+calcTime(+8)+" 星期" + "一二三四五六天".charAt(now.getDay());
+ content += week;
+ for (var i in areas) {
+  var u = valPlus(areas[i][1]);
         content += "\n"+areas[i][0]+" "+calcTime(u)+" UTC" + u;
     }
-	
-    $notify('当前时间', "", content);
+ 
+    $notify('今日时间', "", content);
     $done();
 }
 
@@ -59,8 +59,8 @@ function calcTime(offset) {
 
 function valPlus(offset) { 
     if(offset == 0)
-    	return "";
+     return "";
     else if (offset > 0)
-    	return "+" + offset;
+     return "+" + offset;
     else return offset;
 }
